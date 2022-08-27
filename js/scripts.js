f518f6e25091709b89b64fa572dad58930a69853
Hans Ellis <bullhorn@gmail.com>
2022-08-27 10:45:35 -0700

    add script to gather user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,40 @@
 window.addEventListener("load", function() {
+  console.log("script executing");
+  
+  
   const form = document.querySelector("form");
-
+  
   form.addEventListener("submit" , function(event) {
     event.preventDefault();
-    function getRadio{
-    const person1 = document.querySelector("input[name='personality']:checked").value;
-    const person2 = document.getElementById("personality2").value;
-    const person3 = document.getElementById("personality3").value;
-    const turtle1 = document.getElementById("turtle1").value;
-    const turtle2 = document.getElementById("turtle2").value;
-    const turtle3 = document.getElementById("turtle3").value;
-    const break1 = document.getElementById("breakfast1").value;
-    const break2 = document.getElementById("breakfast2").value;
-    const break3 = document.getElementById("breakfast3").value;
-    const bizkit1 = document.getElementById("bizkit1").value;
-    const bizkit2 = document.getElementById("bizkit2").value;
-    const bizkit3 = document.getElementById("bizkit3").value;
-    const hair1 = document.getElementById("hairstyle1").value;
-    const hair2 = document.getElementById("hairstyle2").value;
-    const hair3 = document.getElementById("hairstyle3").value;
+    const personality = document.querySelector("input[name='personality']:checked").value;
+    console.log(personality);
+    const turtle = document.querySelector("input[name='turtle']:checked").value;
+    console.log(turtle);
+    const breakfast = document.querySelector("input[name='breakfast']:checked").value;
+    console.log(breakfast);
+    const bizkit = document.querySelector("input[name='bizkit']:checked").value;
+    console.log(bizkit);
+    const hair = document.querySelector("input[name='hairstyle']:checked").value;
+    console.log(hair);
     
-    }
+      
 
     window.addEventListener("load" , function() {
-      this.document.getElementById("radio-form").addEventListener("submit", getRadio);
-    })
-}
-  )
+      document.getElementById("radio-form").addEventListener("submit", handlePersonality);
+      
+    });
+    
+    
+    
 
-});
+  
+
+ 
+
+    
+
+  
+  })});
+
 
   
