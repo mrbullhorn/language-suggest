7554bcc3b073fb0482de18b14d43944c2c539fd6
Hans Ellis <bullhorn@gmail.com>
2022-09-03 13:07:54 -0700

    finalize page styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,16 @@
 window.addEventListener("load", function() {
-  console.log("script executing");
-  
   
   const form = document.querySelector("form");
-  
   form.addEventListener("submit" , function(event) {
     event.preventDefault();
     const personality = document.querySelector("input[name='personality']:checked").value;
-    console.log(personality);
     const turtle = document.querySelector("input[name='turtle']:checked").value;
-    console.log(turtle);
     const breakfast = document.querySelector("input[name='breakfast']:checked").value;
-    console.log(breakfast);
     const bizkit = document.querySelector("input[name='bizkit']:checked").value;
-    console.log(bizkit);
     const hair = document.querySelector("input[name='hairstyle']:checked").value;
-    console.log(hair);
   
     let answerHeading = document.getElementById("recHeader");
-    answerHeading.removeAttribute("class");
-    
-    
+    answerHeading.removeAttribute("class" , "hidden");
     let answerOne = document.getElementById("answer1");
     answerOne.setAttribute("class", "hidden");
     let answerTwo = document.getElementById("answer2");
